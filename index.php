92c9f650a0ed9a5e348e7ff35a75555a0d47e90a
fascto <fascto.05@gmail.com>
2024-11-20 20:03:02 -0300

    Switch  to PHP

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php 
+
+// Diccionario 
+$contacts = [
+  ["name" => "Pepe" ,"phonenumber" => "480403433"],
+  ["name" => "Pepito" ,"phonenumber" => "32894832"],
+  ["name" => "Papon" ,"phonenumber" => "480450000"],
+];
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -59,38 +71,20 @@
   <main>
     <div class="container pt-4 p-3">
       <div class="row">
-        <div class="col-md-4 mb-3">
-          <div class="card text-center">
-            <div class="card-body">
-              <h3 class="card-title text-capitalize">Contact Name 1</h3>
-              <p class="m-2">987654321</p>
-              <a href="#" class="btn btn-secondary mb-2">Edit Contact</a>
-              <a href="#" class="btn btn-danger mb-2">Delete Contact</a>
-            </div>
-          </div>
-        </div>
 
+      <?php foreach ($contacts as $contact) :?>
         <div class="col-md-4 mb-3">
           <div class="card text-center">
             <div class="card-body">
-              <h3 class="card-title text-capitalize">Contact Name 2</h3>
-              <p class="m-2">987654321</p>
+              <h3 class="card-title text-capitalize"><?= $contact["name"] ?></h3>
+              <p class="m-2"><?= $contact["phonenumber"] ?></p>
               <a href="#" class="btn btn-secondary mb-2">Edit Contact</a>
               <a href="#" class="btn btn-danger mb-2">Delete Contact</a>
             </div>
           </div>
         </div>
+      <?php endforeach ?>
 
-        <div class="col-md-4 mb-3">
-          <div class="card text-center">
-            <div class="card-body">
-              <h3 class="card-title text-capitalize">Contact Name 3</h3>
-              <p class="m-2">987654321</p>
-              <a href="#" class="btn btn-secondary mb-2">Edit Contact</a>
-              <a href="#" class="btn btn-danger mb-2">Delete Contact</a>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   </main>
